82cab6d4bc47f62336aba8da5c922fd06dddf586
santoshsp2000 <sspatil2017k@gmail.com>
2022-11-04 21:56:06 +0530

    Committing code for Sort Array of 0's, 1's and 2's

---
+++ Python/Array and Matrix/Sort_Array_Of_0_1_2.py
@@ -0,0 +1,18 @@
+class Solution:
+
+    def sort_arry012(self, nums):
+        """ Do not return anything, modify nums in-place instead. """
+
+        start, mid = 0, 0
+        end = len(nums) - 1
+
+        while mid <= end:
+            if nums[mid] == 0:
+                nums[start], nums[mid] = nums[mid], nums[start]
+                mid += 1
+                start += 1
+            elif nums[mid] == 2:
+                nums[mid], nums[end] = nums[end], nums[mid]
+                end -= 1
+            else:
+                mid += 1
