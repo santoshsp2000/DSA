d03916af204c0c2d2f234b41e83b0e95c9cd5762
santoshsp2000 <sspatil2017k@gmail.com>
2022-11-04 21:40:18 +0530

    Committing code for Kadane's Algorithm

---
+++ Python/Array and Matrix/Kadane’s_algorithm.py
@@ -0,0 +1,16 @@
+class Solution:
+
+    def maxSubArray(self, nums):
+        summ = 0
+        maxx = nums[0]
+
+        for i in range(len(nums)):
+            summ += nums[i]
+
+            if summ > maxx:
+                maxx = summ
+
+            if summ < 0:
+                summ = 0
+
+        return maxx
