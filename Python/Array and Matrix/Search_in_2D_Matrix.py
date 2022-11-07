796241cbd5437987fd773a806dc85a9808e06965
santoshsp2000 <sspatil2017k@gmail.com>
2022-11-07 23:12:53 +0530

    Committing code for Search in a 2D Matrix

---
+++ Python/Array and Matrix/Search_in_2D_Matrix.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        n = len(matrix)
+        m = len(matrix[0])
+
+        i = 0
+        j = m - 1
+
+        while i < n and j >= 0:
+            if matrix[i][j] == target:
+                return True
+            elif matrix[i][j] < target:
+                i += 1
+            else:
+                j -= 1
+        return False
