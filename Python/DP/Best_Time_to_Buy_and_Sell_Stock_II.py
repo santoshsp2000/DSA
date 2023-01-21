4c46e4e317acbcfbd8af47e6fed7b0d64a8796c6
santoshsp2000 <sspatil2017k@gmail.com>
2023-01-21 22:58:46 +0530

    Committing code for Best Time to Buy and Sell Stock II

---
+++ Python/DP/Best_Time_to_Buy_and_Sell_Stock_II.py
@@ -0,0 +1,13 @@
+# 122. Best Time to Buy and Sell Stock II ---- leetcode
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        n = len(prices)
+        ans = 0
+
+        for i in range(1, n):
+            if prices[i] > prices[i-1]:
+                ans += prices[i] - prices[i-1]
+        return ans
