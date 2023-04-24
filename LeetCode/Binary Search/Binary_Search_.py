2e435d028c29915c9fee70c4415a7509609d9abf
santoshsp2000 <sspatil2017k@gmail.com>
2023-04-24 22:36:30 +0530

    Binary Search - 24 April 2023

---
+++ LeetCode/Binary Search/Binary_Search_.py
@@ -0,0 +1,21 @@
+# 704. Binary Search
+# Leetcode Easy
+# Date: 24 April 2023
+# TC: O(log(N))
+# SC: O(1)
+from typing import List
+
+
+class Solution:
+
+    def search(self, nums: List[int], target: int) -> int:
+        l = 0
+        h = len(nums)-1
+
+        while l <= h:
+            mid = (l+h)>>1
+            if nums[mid] == target: return mid
+            elif nums[mid] > target: h = mid-1
+            else: l = mid+1
+
+        return -1
