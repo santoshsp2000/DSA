2f885a18ef880d0d1808c3ff12affd2bf7b9a189
santoshsp2000 <sspatil2017k@gmail.com>
2023-05-10 22:30:36 +0530

    Longest Palindromic Subsequence - 14 April 2023

---
+++ LeetCode/DailyCodingChallenge/April 2023/Longest_Palindromic_Subsequence.py
@@ -0,0 +1,22 @@
+# 516. Longest Palindromic Subsequence
+# Leetcode Medium
+# Date: 13 April 2023
+# TC: O(N^2)
+# SC: O(N^2)
+
+
+class Solution:
+
+    def longestPalindromeSubseq(self, s: str) -> int:
+        n = len(s)
+        dp = [[0] * n for _ in range(n)]
+
+        for i in range(n - 1, -1, -1):
+            dp[i][i] = 1
+            for j in range(i + 1, n):
+                if s[i] == s[j]:
+                    dp[i][j] = 2 + dp[i + 1][j - 1]
+                else:
+                    dp[i][j] = max(dp[i + 1][j], dp[i][j - 1])
+
+        return dp[0][n - 1]
