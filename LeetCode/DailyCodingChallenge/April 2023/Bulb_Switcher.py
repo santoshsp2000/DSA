df11747660246588f21c520c23698197d1ef9cd0
santoshsp2000 <sspatil2017k@gmail.com>
2023-05-17 23:41:21 +0530

    Bulb Switcher - 27 April 2023

---
+++ LeetCode/DailyCodingChallenge/April 2023/Bulb_Switcher.py
@@ -0,0 +1,12 @@
+# 319. Bulb Switcher
+# Leetcode Medium
+# Date: 27 April 2023
+# TC: O(1)
+# SC: O(1)
+
+
+class Solution:
+
+    def bulbSwitch(self, n: int) -> int:
+
+        return int(n**0.5)
