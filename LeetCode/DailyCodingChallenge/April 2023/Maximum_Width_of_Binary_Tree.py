2da45337a3d8850ed70eeac56b7bad5e74d90f9e
santoshsp2000 <sspatil2017k@gmail.com>
2023-05-17 19:43:20 +0530

    Maximum Width of Binary Tree - 20 April 2023

---
+++ LeetCode/DailyCodingChallenge/April 2023/Maximum_Width_of_Binary_Tree.py
@@ -0,0 +1,42 @@
+# 662. Maximum Width of Binary Tree
+# Leetcode Medium
+# Date: 20 April 2023
+# TC: O(N)
+# SC: O(N)
+from typing import Optional
+from collections import deque
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+
+    def widthOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        queue = deque([(root, 0)])
+        max_width = 0
+
+        while queue:
+            level_length = len(queue)
+            _, level_start = queue[0]
+
+            for i in range(level_length):
+                node, index = queue.popleft()
+
+                if node.left:
+                    queue.append((node.left, 2 * index))
+
+                if node.right:
+                    queue.append((node.right, 2 * index + 1))
+
+            max_width = max(max_width, index - level_start + 1)
+
+        return max_width
