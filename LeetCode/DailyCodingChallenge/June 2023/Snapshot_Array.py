e2231234203ce867dbd0cd5f037762e92f610197
santoshsp2000 <sspatil2017k@gmail.com>
2023-06-11 22:28:22 +0530

    Snapshot Array - 11 June 2023

---
+++ LeetCode/DailyCodingChallenge/June 2023/Snapshot_Array.py
@@ -0,0 +1,33 @@
+# 1146. Snapshot Array
+# Leetcode Medium
+# Date: 11 June 2023
+# TC: O(log(N))
+# SC: O(1)
+
+
+class SnapshotArray:
+
+    def __init__(self, length: int):
+        self.array = [[(0, 0)] for _ in range(length)]
+        self.snap_id = 0
+
+    def set(self, index: int, val: int) -> None:
+        self.array[index].append((self.snap_id, val))
+
+    def snap(self) -> int:
+        self.snap_id += 1
+
+        return self.snap_id - 1
+
+    def get(self, index: int, snap_id: int) -> int:
+        history = self.array[index]
+        left, right = 0, len(history) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+            if history[mid][0] <= snap_id:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return history[right][1]
