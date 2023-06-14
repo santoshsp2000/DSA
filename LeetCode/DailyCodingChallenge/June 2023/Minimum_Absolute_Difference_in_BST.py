779ff3a16021acf5b348b0597c92a554c71dfafa
santoshsp2000 <sspatil2017k@gmail.com>
2023-06-14 21:48:15 +0530

    Minimum Absolute Difference in BST - 14 June 2023

---
+++ LeetCode/DailyCodingChallenge/June 2023/Minimum_Absolute_Difference_in_BST.py
@@ -0,0 +1,42 @@
+# 530. Minimum Absolute Difference in BST
+# Leetcode Easy
+# Date: 14 June 2023
+# TC: O(N)
+# SC: O(1)
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+
+    def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
+
+        prev = float('-inf')
+        min_diff = float('inf')
+
+        def inorderTraversal(root):
+            nonlocal prev, min_diff
+            if not root:
+                return
+
+            if root.left:
+                inorderTraversal(root.left)
+
+            if (root.val - prev) < min_diff:
+                min_diff = root.val - prev
+
+            prev = root.val
+
+            if root.right:
+                inorderTraversal(root.right)
+
+        inorderTraversal(root)
+
+        return min_diff
