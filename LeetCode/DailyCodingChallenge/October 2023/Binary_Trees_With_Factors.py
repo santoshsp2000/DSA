c9a43b9e9a0a964e3452775cfc00f49461743f33
santoshsp2000 <sspatil2017k@gmail.com>
2023-10-26 22:04:05 +0530

    Binary Trees With Factors - 26 October 2023

--- LeetCode/DailyCodingChallenge/October 2023/Binary_Trees_With_Factors.py
+++ LeetCode/DailyCodingChallenge/October 2023/Binary_Trees_With_Factors.py
@@ -12,12 +12,15 @@ MOD = 10 ** 9 + 7
 class Solution:
 
     def numFactoredBinaryTrees(self, arr: List[int]) -> int:
+
         arr.sort()
         s = set(arr)
         dp = {x: 1 for x in arr}
 
         for i in arr:
+
             for j in arr:
+
                 if j > i ** 0.5:
                     break
                 if i % j == 0 and i // j in s:
